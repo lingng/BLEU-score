96361266dea37519c29613e68dd502806533958c
Yuting Zhang <lizlingng@gmail.com>
2016-04-25 10:09:06 -0700

    Get clipped count for unigram, bigram, trigram and fourgram

--- calculatebleu.py
+++ calculatebleu.py
@@ -28,18 +28,31 @@ def get_clipped_count(candidate_dic, reference_dic):
 
 	return clipped_dic
 
+"""
+	Generate ngrams for candidate and reference.
+	Use / to separate each word to generate key for the dictionary. # List is not hashable
+
+	@candidate: candidate translation sentence
+	@reference: reference translation sentence
+	@n: number for the ngram
+	return: [can_dic, ref_dic, can_len]
+		can_dic: candidate ngram count dictionary
+		ref_dic: reference ngram count dictionary
+		can_len: the number of ngrams generated from the candidate translation
+"""
 def generate_ngram(candidate, reference, n):
 	can_dic = {}
 	ref_dic = {}
 	can_list = candidate.split(" ")
 	ref_list = reference.split(" ")
+	can_len = 0
 	
 	for i in range(0, len(can_list)-n+1):
 		key = ""
 		for j in range(0, n):
 			key += can_list[i+j]
 			key += "/"
-
+		can_len += 1
 		if can_dic.has_key(key):
 			can_dic[key] += 1
 		else:
@@ -55,17 +68,28 @@ def generate_ngram(candidate, reference, n):
 			ref_dic[key] += 1
 		else:
 			ref_dic[key] = 1
-	clipped_dic = get_clipped_count(can_dic, ref_dic)
-	print clipped_dic
+	return can_dic, ref_dic, can_len
 
 
 def get_count(candidate, reference):
 	candidate = candidate.strip()
 	reference = reference.strip()
-	uni_c = generate_ngram(candidate, reference, 2)
-	# bi_c = bigram(candidate, reference)
-	# tri_c = trigram(candidate, reference)
-	# four_c = fourgram(candidate, reference)
+	uni_c_dic, uni_r_dic, uni_c_len = generate_ngram(candidate, reference, 1)
+	uni_clipped_dic = get_clipped_count(uni_c_dic, uni_r_dic)
+
+	bi_c_dic, bi_r_dic, bi_c_len = generate_ngram(candidate, reference, 2)
+	bi_clipped_dic = get_clipped_count(bi_c_dic, bi_r_dic)
+
+	tri_c_dic, tri_r_dic, tri_c_len = generate_ngram(candidate, reference, 3)
+	tri_clipped_dic = get_clipped_count(tri_c_dic, tri_r_dic)
+
+	four_c_dic, four_r_dic, four_c_len = generate_ngram(candidate, reference, 4)
+	four_clipped_dic = get_clipped_count(four_c_dic, four_r_dic)
+	print uni_clipped_dic
+	print bi_clipped_dic
+	print tri_clipped_dic
+	print four_clipped_dic
+	print "----"
 	# penalty = brevity_penalty(candidate, reference)
 
 def main():
